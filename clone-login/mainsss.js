cbff34a3dd4d60856544c0ef49352bcaabb6086e
isafuyu <kienminh.tnut@hotmail.com>
2022-06-05 23:06:31 +0700

    login-facebook

---
+++ clone-login/mainsss.js
@@ -0,0 +1,9 @@
+
+        const showResai = document.querySelector('.show-now');
+
+        function show_resai_1() {
+            showResai.className="show-item";
+        }
+        function remover_1() {
+            showResai.classList.toggle("show-now");
+        }
